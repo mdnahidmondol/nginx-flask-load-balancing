a8a1046e1e70a5203f2185fa0b3a860651da28b9
mdnahidmondol-kali <md.mondol@graduate.utm.my>
2021-09-04 05:04:02 -0400

    app2 running without issue

--- app1/app.py
+++ app1/app.py
@@ -6,7 +6,7 @@ app1 = Flask(__name__)
 @app1.route('/')
 
 def hello_world():
-    return 'Salam alikom, this is app1 :) '
+    return 'Alsalam Alykom, this is app1 :) '
 
 if __name__ == '__main__':
     app1.run(debug=True, host='0.0.0.0')
